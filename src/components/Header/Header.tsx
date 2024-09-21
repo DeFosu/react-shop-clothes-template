488df2a03676c24f2aa0030258e80f83024be5a5
Yarik <defosu.33@gmail.com>
2024-09-21 10:29:09 +0300

    feat: create base scheme for Search form

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 import React from "react";
 import LinkList from "./LinkList";
 import IconWithNumbers from "./IconWithNumbers";
@@ -5,6 +7,39 @@ import NavigationMenu from "./NavigationMenu";
 
 const Header: React.FC = () => (
     <header className="w-full bg-neutral-950 text-neutral-100">
+        <div className="absolute grid w-full h-full bg-neutral-800 bg-opacity-60 place-content-center">
+            <div>
+                <div className="flex justify-between font-semibold text-neutral-100">
+                    <h2 className="text-3xl">Search</h2>
+                    <button className="p-0.5">
+                        <AiOutlineClose size={26} />
+                    </button>
+                </div>
+                <form
+                    action=""
+                    className="flex items-center mt-4 overflow-hidden border-2 rounded border-neutral-900"
+                >
+                    <select
+                        name="gender"
+                        className="px-6 text-2xl h-14 text-neutral-900 bg-neutral-200"
+                    >
+                        <option value="all">All Genders</option>
+                        <option value="man">Man</option>
+                        <option value="woman">Woman</option>
+                    </select>
+                    <div className="border h-14 border-neutral-900"></div>
+                    <input
+                        type="search"
+                        name="search"
+                        className="pl-6 text-2xl h-14 text-neutral-900 placeholder-neutral-400 bg-neutral-200 focus:outline-none"
+                        placeholder="Search..."
+                    />
+                    <button className="grid h-14 place-content-center aspect-square bg-neutral-200 text-neutral-900">
+                        <AiOutlineSearch size={24} />
+                    </button>
+                </form>
+            </div>
+        </div>
         <div className="w-full bg-neutral-200">
             <div className="container px-4 mx-auto">
                 <div className="flex items-center justify-between py-1 text-sm font-medium text-neutral-500">
@@ -34,8 +69,10 @@ const Header: React.FC = () => (
                             </button>
                         </div>
                         <div className="flex items-center gap-2 text-sm">
-                            <button>Btn-1</button>
                             <button>Btn-2</button>
+                            <button className="p-0.5">
+                                <AiOutlineSearch size={20} />
+                            </button>
                             <button>Btn-3</button>
                             <button>Btn-4</button>
                             <button>Btn-5</button>
