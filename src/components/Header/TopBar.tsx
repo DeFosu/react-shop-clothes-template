9b6ecd361ef0bd76d3f8b829443c230da9187965
Yarik <defosu.33@gmail.com>
2024-09-22 10:47:09 +0300

    refactor: remove unnecessary container inside Container component in TopBar

--- src/components/Header/TopBar.tsx
+++ src/components/Header/TopBar.tsx
@@ -6,17 +6,13 @@ import Container from "../Container/Container";
 const TopBar: React.FC = () => {
     return (
         <div className="w-full bg-neutral-200">
-            <Container>
-                <div className="flex items-center justify-between py-1 text-sm font-medium text-neutral-500">
-                    <LinkList
-                        links={["Link-1", "Link-2", "Link-3", "Link-4"]}
+            <Container className="flex items-center justify-between py-1 text-sm font-medium text-neutral-500">
+                <LinkList links={["Link-1", "Link-2", "Link-3", "Link-4"]} />
+                <div className="flex items-center gap-8">
+                    <IconWithNumbers
+                        items={["Icon", "Number", "Number", "Number"]}
                     />
-                    <div className="flex items-center gap-8">
-                        <IconWithNumbers
-                            items={["Icon", "Number", "Number", "Number"]}
-                        />
-                        <IconWithNumbers items={["Icon", "Email"]} />
-                    </div>
+                    <IconWithNumbers items={["Icon", "Email"]} />
                 </div>
             </Container>
         </div>
