df3765d59c559b93dff84495a478bfad658ce384
Yarik <defosu.33@gmail.com>
2024-09-22 09:14:31 +0300

    feat: add className parameter to Container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,10 +2,13 @@ import React, { type ReactNode } from "react";
 
 type ContainerType = {
     children: ReactNode;
+    className?: string;
 };
 
-const Container: React.FC<ContainerType> = ({ children }) => {
-    return <div className="container px-4 mx-auto">{children}</div>;
+const Container: React.FC<ContainerType> = ({ children, className = "" }) => {
+    return (
+        <div className={`container px-4 mx-auto ${className}`}>{children}</div>
+    );
 };
 
 export default Container;
