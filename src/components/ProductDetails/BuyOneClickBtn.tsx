1460cb84c17977ea33b05dc435b8e91529f92fd4
Yarik <defosu.33@gmail.com>
2024-10-02 21:52:23 +0300

    feat: add variant styles for Button

--- src/components/ProductDetails/BuyOneClickBtn.tsx
+++ src/components/ProductDetails/BuyOneClickBtn.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { type ReactNode } from "react";
 
+type TButton = {
+    children: ReactNode;
+    className?: string;
+    variant?: "text" | "contained" | "outlined";
+};
 
+const BuyOneClickBtn: React.FC<TButton> = (props) => {
+    const { children, className = "", variant = "contained" } = props;
 
-const BuyOneClickBtn = () => {
+    const styleVariant = {
+        text: "underline",
+        contained: "bg-gray-400 text-black font-normal text-xs py-4 px-8",
+        outlined:
+            " text-black font-normal text-xs py-4 px-8 bg-transparent border",
+    };
 
-  return (
-    <button className="bg-gray-400 text-black font-normal text-xs py-4 px-8">
-      BUY IN ONE CLICK
-    </button>
-  );
+    const buttonStyle = `${styleVariant[variant]} ${className}`;
+
+    return <button className={buttonStyle}>{children}</button>;
 };
 
 export default BuyOneClickBtn;
--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,44 +1,44 @@
-import React, {useState} from 'react';
-import ToBasketBtn from './ToBasketBtn';
-import BuyOneClickBtn from './BuyOneClickBtn';
-import Sizes from './Sizes';
-import InfoButton from './InfoButton';
-
-
+import React, { useState } from "react";
+import ToBasketBtn from "./ToBasketBtn";
+import BuyOneClickBtn from "./BuyOneClickBtn";
+import Sizes from "./Sizes";
+import InfoButton from "./InfoButton";
 
 const ProductDetails = () => {
-  const hasDiscount = false;
-const [value, setValue] = useState('❤');
+    const hasDiscount = false;
+    const [value, setValue] = useState("❤");
 
-const handleClick = () => {
-    setValue((prevValue) => (prevValue === '❤' ? '🖤' : '❤'));
-};
+    const handleClick = () => {
+        setValue((prevValue) => (prevValue === "❤" ? "🖤" : "❤"));
+    };
 
-  // Функция для расчета бонуса (3% от текущей цены)
-  // const calculateBonus = (price) => {
-  //   return (price * 0.03).toFixed(2); 
-  // }; 
-  return (
-    <div className="mx-auto ">
-       <div className="pb-6 text-xs font-normal text-gray-400 flex items-center space-x-4">
-      <span>Для хлопців</span>
-      <span className="text-gray-300">—</span>
-      <span>Взуття</span>
-      <span className="text-gray-300">—</span>
-      <span className="text-black">Кросівки, кеди</span>
-    </div>
-      <div className="text-base font-semibold">
-        Кросівки Staff white & brown
-        <button
-                    className=" w-8 h-8 text-red-300 top-2 right-2 rounded-full"
+    // Функция для расчета бонуса (3% от текущей цены)
+    // const calculateBonus = (price) => {
+    //   return (price * 0.03).toFixed(2);
+    // };
+    return (
+        <div className="mx-auto ">
+            <div className="flex items-center pb-6 space-x-4 text-xs font-normal text-gray-400">
+                <span>Для хлопців</span>
+                <span className="text-gray-300">—</span>
+                <span>Взуття</span>
+                <span className="text-gray-300">—</span>
+                <span className="text-black">Кросівки, кеди</span>
+            </div>
+            <div className="text-base font-semibold">
+                Кросівки Staff white & brown
+                <button
+                    className="w-8 h-8 text-red-300 rounded-full top-2 right-2"
                     onClick={handleClick}
                 >
                     {value}
                 </button>
-      </div>
+            </div>
 
-      <span className="py-1 space-x-2 text-xs text-gray-400">Article: MS0276 Category: Shoes Category: Shoes</span> 
-      <div className="pb-4">
+            <span className="py-1 space-x-2 text-xs text-gray-400">
+                Article: MS0276 Category: Shoes Category: Shoes
+            </span>
+            <div className="pb-4">
                 <span
                     className={`inline-block text-xl font-medium ${
                         hasDiscount && "text-red-500"
@@ -52,17 +52,18 @@ const handleClick = () => {
                     </span>
                 )}
             </div>
-        <p className="text-gray-400 mt-1">Бонуси: 24 грн</p>                   
-        {/* сделать ологику выщета бонуса в размере 3 проц от цены*/}
-        <div className=''>
-          <Sizes />
-<ToBasketBtn />
-<BuyOneClickBtn />
-<InfoButton />
-  </div>
-   </div>
-
-  );
+            <p className="mt-1 text-gray-400">Бонуси: 24 грн</p>
+            {/* сделать ологику выщета бонуса в размере 3 проц от цены*/}
+            <div className="">
+                <Sizes />
+                <ToBasketBtn />
+                <BuyOneClickBtn variant="contained" className="bg-red-800">
+                    BUY IN ONE CLICK
+                </BuyOneClickBtn>
+                <InfoButton />
+            </div>
+        </div>
+    );
 };
 
 export default ProductDetails;
